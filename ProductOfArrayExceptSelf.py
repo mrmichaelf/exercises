a83e0cfd4b3baa4d50da0f715cfb49fda0ddb78c
mrmichaelf <mrfrenkelmichael@gmail.com>
2021-08-08 18:24:08 +0300

    Third commit

--- ProductOfArrayExceptSelf.py
+++ ProductOfArrayExceptSelf.py
@@ -1,5 +1,37 @@
+from sys import stdin
 
+
+def multiply(myList):
+    result = 1
+    for x in myList:
+        result = result * x
+    return result
+
+
+def getProductArrayExceptSelf(arr, n):
+    return [multiply(arr[:i] + arr[i + 1:]) for i, x in enumerate(arr)]
+
+
+def takeInput():
+    n = int(stdin.readline().rstrip())
+
+    if n == 0:
+        return list(), 0
+
+    arr = list(map(int, stdin.readline().rstrip().split(" ")))
+    return arr, n
+
+
+def printList(arr, n):
+    for i in range(n):
+        print(arr[i], end=" ")
+    print()
 
 
 if __name__ == "__main__":
-    print(77)
+    t = int(stdin.readline().rstrip())
+    while t > 0:
+        arr, n = takeInput()
+        product = getProductArrayExceptSelf(arr, n)
+        printList(product, n)
+        t -= 1
