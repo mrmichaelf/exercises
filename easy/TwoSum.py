60159f41c192dea0b00f62c1dbdb8828d4aefff4
mrmichaelf <mrfrenkelmichael@gmail.com>
2021-08-09 17:10:55 +0300

    Two Sum

---
+++ easy/TwoSum.py
@@ -0,0 +1,32 @@
+def two_sum(arr, target):
+    result = []
+    d = {}  # key - number, value = number of occurrences
+    for x in arr:
+        val = target - x
+        if val in d:
+            result.extend([[str(val), str(x)]])
+            d[val] -= 1
+            if d[val] == 0:
+                del d[val]
+        elif x in d:
+            d[x] += 1
+        else:
+            d[x] = 1
+    if len(result) == 0:
+        result = [['-1', '-1']]
+    return result
+
+
+def print_ans(m_ans):
+    for y in m_ans:
+        if y[0] < y[1]:
+            print('{} {}'.format(y[0], y[1]))
+        else:
+            print('{} {}'.format(y[1], y[0]))
+
+
+if __name__ == "__main__":
+    inputs = ([9, [2, 7, 11, 13]], [1, [1, -1, -1, 2, 2]])
+    for input_list in inputs:
+        ans = two_sum(input_list[1], input_list[0])
+        print_ans(ans)
